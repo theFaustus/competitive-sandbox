d3d109589ad6c1dee0eebe0099b1714d5a02aa7b
ipascari <jhoonnyc@gmail.com>
2021-01-10 20:46:58 +0200

    fix some bugs, add more test cases

--- src/main/java/com/evil/inc/EnoughIsEnough.java
+++ src/main/java/com/evil/inc/EnoughIsEnough.java
@@ -5,15 +5,24 @@ import java.util.Arrays;
 public class EnoughIsEnough {
 
     public static int[] deleteNth(int[] elements, int maxOccurrences) {
+
+        if (maxOccurrences == 0) {
+            elements = new int[]{};
+        }
         int currentElementOccurrences = 0;
         for (int i = 0; i < elements.length; i++) {
             currentElementOccurrences = 0;
             for (int j = 0; j < elements.length; j++) {
+                if (i >= elements.length)
+                    continue;
                 if (elements[i] == elements[j] && i != j) {
                     currentElementOccurrences++;
                     if (currentElementOccurrences >= maxOccurrences) {
+                        System.out.println(elements[i] + "@" + i);
+                        System.out.println(elements[j] + "@" + j);
                         elements = removeElement(elements, j);
                         System.out.println(Arrays.toString(elements));
+                        --j; //decrease the index after removing
                     }
                 }
             }
@@ -22,10 +31,10 @@ public class EnoughIsEnough {
     }
 
     public static int[] removeElement(int[] original, int element) {
-        int[] n = new int[original.length - 1];
-        System.arraycopy(original, 0, n, 0, element);
-        System.arraycopy(original, element + 1, n, element, original.length - element - 1);
-        return n;
+        int[] copy = new int[original.length - 1];
+        System.arraycopy(original, 0, copy, 0, element);
+        System.arraycopy(original, element + 1, copy, element, original.length - element - 1);
+        return copy;
     }
 
 }
