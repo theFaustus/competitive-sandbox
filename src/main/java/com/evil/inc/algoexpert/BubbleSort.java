bae47217c265ce565bc09b1635eaebdb4f67c862
ipascari <jhoonnyc@gmail.com>
2021-09-02 21:07:56 +0300

    solve some algoexpert problems

---
+++ src/main/java/com/evil/inc/algoexpert/BubbleSort.java
@@ -0,0 +1,32 @@
+package com.evil.inc.algoexpert;
+
+public class BubbleSort {
+    /*Bubble Sort
+Write a function that takes in an array of integers and returns a sorted version of that array. Use the Bubble Sort algorithm to sort the array.
+
+If you're unfamiliar with Bubble Sort, we recommend watching the Conceptual Overview section of this question's video explanation before starting to code.
+
+Sample Input
+array = [8, 5, 2, 9, 5, 6, 3]
+Sample Output
+[2, 3, 5, 5, 6, 8, 9]
+    * */
+    public static int[] bubbleSort(int[] array) {
+        boolean isSorted = false;
+        int lockedIndex = array.length;
+        while (!isSorted) {
+            isSorted = true;
+            for (int i = 0, j = 1; i < lockedIndex - 1; i++, j++) {
+                if (array[i] > array[j]) {
+                    int temp = array[j];
+                    array[j] = array[i];
+                    array[i] = temp;
+                    isSorted = false;
+                }
+            }
+            lockedIndex--;
+        }
+        return array;
+    }
+
+}
